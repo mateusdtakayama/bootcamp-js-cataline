1c4d5bf543f82b4570201ceff57a22b31a2699b2
Mateus Delai <mateus.takayama@ssys.com.br>
2023-05-10 09:35:26 -0300

    add: babel npm run dev

--- 2.ecmascript/public/bundle.js
+++ 2.ecmascript/public/bundle.js
@@ -59,3 +59,56 @@ var anything = function anything() {
   return [1, 2, 3, 4, 5];
 };
 console.log(anything());
+
+// parametro padrao
+
+var SayHello = function SayHello() {
+  var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : "Denis";
+  return console.log("ola " + name);
+};
+SayHello();
+
+// template string
+
+var showProduct = function showProduct(product, price) {
+  console.log("O valor da ".concat(product, ", custa R$ ").concat(price));
+};
+showProduct("Camisa", 15);
+
+// objetct short syntax
+
+var name = "Pedro";
+var age = 15;
+var person = {
+  name: name,
+  age: age,
+  alive: true
+};
+console.log(person);
+
+// desestruturacao
+
+var fruits = ['maça', 'banana', 'pera', 'abacate'];
+var a = fruits[0],
+  b = fruits[1],
+  c = fruits[3];
+console.log(a, b, c);
+var city = {
+  cidade: "Toledo",
+  state: "Paraná",
+  uf: "PR",
+  adress: {
+    street: "Rua da faculdade",
+    number: "1113"
+  }
+};
+var cidade = city.cidade,
+  state = city.state,
+  street = city.adress.street;
+console.log(cidade, state, street);
+var showState = function showState(_ref) {
+  var cidade = _ref.cidade,
+    state = _ref.state;
+  console.log("A cidade ".concat(cidade, " \xE9 do estado ").concat(state));
+};
+showState(city);
